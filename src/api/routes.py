3dc1b513a3a7511071b7d1f9c9e2a57b4e5cd795
andrey.lesnov <andreythecoolguy@gmail.com>
2026-05-30 13:47:01 +0300

    feat: add api details - response models, status codes add query params, request/response bodies, service calling

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,30 +1,50 @@
 import uuid
 
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Depends, Query
+
+from src.dto import TaskRequest, PaginatedResponse, TaskResponse
+from src.models import TaskStatus
+from src.service import TaskService
 
 task_router = APIRouter(prefix="/tasks", tags=["tasks"])
 
 
-@task_router.post("/")
-async def create_task():
-    pass
+@task_router.post("/", response_model=TaskResponse, status_code=status.HTTP_201_CREATED)
+async def create_task(
+    request: TaskRequest,
+    service: TaskService = Depends(TaskService),
+):
+    return await service.create_task(request)
 
 
-@task_router.get("/")  # todo add pagination
-async def get_tasks():
-    pass
+@task_router.get("/", response_model=PaginatedResponse[TaskResponse])
+async def get_tasks(
+    page: int = Query(default=1, ge=1),
+    limit: int = Query(default=10, le=10),
+    service: TaskService = Depends(TaskService),
+):
+    return await service.get_task_list(page=page, limit=limit)
 
 
-@task_router.get("/{task_id:uuid}")
-async def get_task_by_id(task_id: uuid.UUID):
-    pass
+@task_router.get("/{task_id:uuid}", response_model=TaskResponse)
+async def get_task_by_id(
+    task_id: uuid.UUID,
+    service: TaskService = Depends(TaskService),
+):
+    return await service.get_task(task_id)
 
 
-@task_router.get("/{task_id:uuid}/status")
-async def get_task_status_by_id(task_id: uuid.UUID):
-    pass
+@task_router.get("/{task_id:uuid}/status", response_model=TaskStatus)
+async def get_task_status_by_id(
+    task_id: uuid.UUID,
+    service: TaskService = Depends(TaskService),
+):
+    return await service.get_task_status(task_id)
 
 
-@task_router.delete("/{task_id:uuid}")
-async def delete_task_by_id(task_id: uuid.UUID):
-    pass
+@task_router.delete("/{task_id:uuid}", status_code=status.HTTP_204_NO_CONTENT)
+async def cancel_task_by_id(
+    task_id: uuid.UUID,
+    service: TaskService = Depends(TaskService),
+):
+    return await service.cancel_task(task_id)
