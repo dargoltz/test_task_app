0d0b33dfdaadf9a188298ae5b7660fbe599a588a
andrey.lesnov <andreythecoolguy@gmail.com>
2026-05-30 13:54:49 +0300

    feat: add mappers

--- src/service/task_service.py
+++ src/service/task_service.py
@@ -1,7 +1,7 @@
 import uuid
 
 from src.dto import TaskResponse, TaskRequest, PaginatedResponse
-from src.models import TaskStatus
+from src.models import TaskStatus, TaskORM
 from src.repository.task_repository import TaskRepository
 
 
@@ -9,20 +9,27 @@ class TaskService:
     def __init__(self, repository: TaskRepository):
         self.repository = repository
 
-    async def create_task(self, task: TaskRequest) -> TaskResponse:
-        task_orm = ...
+    @staticmethod
+    def _orm_to_response(task: TaskORM) -> TaskResponse:
+        return TaskResponse.model_validate(task)
 
+    @staticmethod
+    def _request_to_orm(request: TaskRequest) -> TaskORM:
+        return TaskORM(**request.model_dump())
+
+    async def create_task(self, task: TaskRequest) -> TaskResponse:
+        task_orm = self._request_to_orm(task)
         created = await self.repository.create(task_orm)
 
-        task_response = ...
+        # todo run task
 
-        return task_response
+        return self._orm_to_response(created)
 
     async def get_task_list(self, page: int, limit: int) -> PaginatedResponse[TaskResponse]:
         task_list = await self.repository.get_list(page, limit)
         task_total = await self.repository.get_total()
 
-        task_responses = ...
+        task_responses = [self._orm_to_response(task) for task in task_list]
 
         return PaginatedResponse(
             page=page,
@@ -34,9 +41,7 @@ class TaskService:
     async def get_task(self, task_id: uuid.UUID) -> TaskResponse:
         task = await self.repository.get_by_id(task_id)
 
-        task_response = ...
-
-        return task_response
+        return self._orm_to_response(task)
 
     async def get_task_status(self, task_id: uuid.UUID) -> TaskStatus:
         task = await self.repository.get_by_id(task_id)
