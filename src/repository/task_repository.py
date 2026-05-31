59812b6fc894ef4dfcdf069c470f676e615c4594
andrey.lesnov <andreythecoolguy@gmail.com>
2026-05-31 17:06:09 +0300

    feat: implement repository

--- src/repository/task_repository.py
+++ src/repository/task_repository.py
@@ -1,24 +1,49 @@
 import uuid
+from sqlalchemy import select, func, update
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.core.db import DBSession
 from src.models import TaskORM, TaskStatus
 
 
 class TaskRepository:
-    def __init__(self, session: DBSession):
+    def __init__(self, session: AsyncSession):
         self.session = session
 
     async def create(self, task: TaskORM) -> TaskORM:
-        pass
+        self.session.add(task)
+        await self.session.flush()
+        return task
 
     async def get_list(self, page: int, limit: int) -> list[TaskORM]:
-        pass
+        offset = (page - 1) * limit
+
+        result = await self.session.execute(
+            select(TaskORM)
+            .offset(offset)
+            .limit(limit)
+            .order_by(TaskORM.created_at.desc())
+        )
+        return list(result.scalars().all())
 
     async def get_total(self) -> int:
-        pass
+        result = await self.session.execute(
+            select(func.count(TaskORM.id))
+        )
+        return result.scalar_one()
 
-    async def get_by_id(self, task_id: uuid.UUID) -> TaskORM:
-        pass
+    async def get_by_id(self, task_id: uuid.UUID) -> TaskORM | None:
+        result = await self.session.execute(
+            select(TaskORM).where(TaskORM.id == task_id)
+        )
+        return result.scalar_one_or_none()
 
-    async def update_status_by_id(self, task_id: uuid.UUID, status: TaskStatus):
-        pass
+    async def update_status_by_id(
+        self,
+        task_id: uuid.UUID,
+        status: TaskStatus,
+    ) -> None:
+        await self.session.execute(
+            update(TaskORM)
+            .where(TaskORM.id == task_id)
+            .values(status=status)
+        )
