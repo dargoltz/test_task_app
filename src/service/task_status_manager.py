fe617c617b67a34a002aaba3311880ccdbdf7ca7
andrey.lesnov <andreythecoolguy@gmail.com>
2026-06-04 14:39:48 +0300

    feat: add new custom exception for task status manager

--- src/service/task_status_manager.py
+++ src/service/task_status_manager.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from src.domain.exceptions import TaskStatusError
 from src.domain.models import Task
 from src.domain.value_objects import TaskStatus
 
@@ -11,14 +12,14 @@ class TaskStatusManager:
     @staticmethod
     def set_pending(task: Task) -> None:
         if task.status not in (TaskStatus.NEW, TaskStatus.FAILED):
-            raise ValueError(f"Can't set task {task.id} as pending: must be NEW or FAILED")
+            raise TaskStatusError(f"Can't set task {task.id} as pending: must be NEW or FAILED")
 
         task.status = TaskStatus.PENDING
 
     @staticmethod
     def start(task: Task) -> None:
         if task.status != TaskStatus.PENDING:
-            raise ValueError(f"Can't start {task.id}: must be PENDING")
+            raise TaskStatusError(f"Can't start {task.id}: must be PENDING")
 
         task.started_at = datetime.now()
         task.status = TaskStatus.IN_PROGRESS
@@ -26,7 +27,7 @@ class TaskStatusManager:
     @staticmethod
     def complete(task: Task, result: str) -> None:
         if task.status != TaskStatus.IN_PROGRESS:
-            raise ValueError(f"Can't complete {task.id}: must be IN_PROGRESS")
+            raise TaskStatusError(f"Can't complete {task.id}: must be IN_PROGRESS")
 
         task.result = result
         task.finished_at = datetime.now()
@@ -41,6 +42,6 @@ class TaskStatusManager:
     @staticmethod
     def cancel(task: Task) -> None:
         if task.status not in (TaskStatus.NEW, TaskStatus.PENDING):
-            raise ValueError(f"Can't cancel {task.id}: must be NEW or PENDING")
+            raise TaskStatusError(f"Can't cancel {task.id}: must be NEW or PENDING")
 
         task.status = TaskStatus.CANCELLED
